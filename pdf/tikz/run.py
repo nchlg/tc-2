9d1a780c94ecebff0117ed2d44ea459df42377e4
pris0213 <priscila.vivian@acad.pucrs.br>
2017-06-21 15:36:57 -0300

    1. Tikz da integração da ontologia 2. Imagens de algumas propriedades da ontologia 3. Capturas de tela da aplicação tiradas (falta adicionar ao texto).

---
+++ pdf/tikz/run.py
@@ -0,0 +1,31 @@
+from subprocess import call
+
+# Compila os .tex
+call(['pdflatex', 'toptop-1.tex'])
+call(['pdflatex', 'topdown-1.tex'])
+call(['pdflatex', 'topdown-2.tex'])
+call(['pdflatex', 'topdown-3.tex'])
+call(['pdflatex', 'topdown-4.tex'])
+call(['pdflatex', 'topdown-5.tex'])
+call(['pdflatex', 'bottomup-1.tex'])
+call(['pdflatex', 'bottomup-2.tex'])
+call(['pdflatex', 'bottomup-3.tex'])
+call(['pdflatex', 'bottomup-4.tex'])
+call(['pdflatex', 'bottomup-5.tex'])
+
+# Faz o crop dos pdfs de saida
+call(['pdfcrop', 'toptop-1.pdf', 'toptop-1.pdf'])
+call(['pdfcrop', 'topdown-1.pdf', 'topdown-1.pdf'])
+call(['pdfcrop', 'topdown-2.pdf', 'topdown-2.pdf'])
+call(['pdfcrop', 'topdown-3.pdf', 'topdown-3.pdf'])
+call(['pdfcrop', 'topdown-4.pdf', 'topdown-4.pdf'])
+call(['pdfcrop', 'topdown-5.pdf', 'topdown-5.pdf'])
+call(['pdfcrop', 'bottomup-1.pdf', 'bottomup-1.pdf'])
+call(['pdfcrop', 'bottomup-2.pdf', 'bottomup-2.pdf'])
+call(['pdfcrop', 'bottomup-3.pdf', 'bottomup-3.pdf'])
+call(['pdfcrop', 'bottomup-4.pdf', 'bottomup-4.pdf'])
+call(['pdfcrop', 'bottomup-5.pdf', 'bottomup-5.pdf'])
+
+# Limpeza
+call(['rm *.aux'], shell=True)
+call(['rm *.log'], shell=True)
